86b1d83d72c9972f1534063267d84ef467ad553f
ckwear <21thpoem@naver.com>
2021-12-09 22:58:25 +0900

    first commit

---
+++ src/main/java/keycloak/dev/keycloakStudy/KeycloakStudyApplication.java
@@ -0,0 +1,13 @@
+package keycloak.dev.keycloakStudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/keycloak/dev/keycloakStudy/KeycloakStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package keycloak.dev.keycloakStudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
